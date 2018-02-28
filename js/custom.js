1a86453f42efe0b034ab40e0db2676340d7286fb
John Weisbrodt <31863920+akadubss@users.noreply.github.com>
2018-02-27 21:20:20 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '5941440362',
+        limit: 12,
+        resolution: 'standard_resolution',
+        accessToken: '5941440362.1677ed0.b1f67c7dc18946c8985c508fd0dd5b67',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-thumbnail"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    $('#wrapper').fullpage();
+    
+    
+
+});
