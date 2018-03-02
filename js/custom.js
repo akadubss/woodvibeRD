4ba4c1c10de227672f010aefcdf99b6f29368352
John Weisbrodt <31863920+akadubss@users.noreply.github.com>
2018-03-01 21:04:52 -0500

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,13 +4,11 @@ $(document).ready(function() {
     var userFeed = new Instafeed({
         get: 'user',
         userId: '5941440362',
-
-        limit: 8,
+        limit: 16,
         resolution: 'standard_resolution',
         accessToken: '5941440362.1677ed0.b1f67c7dc18946c8985c508fd0dd5b67',
         sortBy: 'most-recent',
-        template: '<div class="col-lg-3 col-md-3 col-sm-6 col-6  instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-thumbnail"/></a></div>',
-
+        template: '<div class="col-lg-2 col-md-2 col-sm-6 col-6  instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-thumbnail"/></a></div>',
     });
 
 
@@ -28,12 +26,10 @@ $(document).ready(function() {
 
     $('#wrapper').fullpage();
     
-
     $('#carouselFade').carousel();
 
+     baguetteBox.run('.tz-gallery');
 });
 
 
-    
 
-});
